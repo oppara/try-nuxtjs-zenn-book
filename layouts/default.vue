5dd7ccdb382f40898610faeef8b798dd4cd57309
oppara <oppara@oppara.tv>
2021-09-18 16:24:23 +0900

    Add layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+      <div class="container">
+        <a class="navbar-brand" href="/">LP SITE</a>
+      </div>
+    </nav>
+    <div class="container">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  .container{
+    max-width: 640px;
+  }
+</style>
